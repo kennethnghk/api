893fb257f7a879a2e0220323947d3fd3bb2cd42a
Kenneth Ng <ng.tszcheong@gmail.com>
2018-06-17 15:59:14 +0800

    Add pre-commit hook

--- node/server.js
+++ node/server.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 8080
+const HOST = '0.0.0.0'
 
 // App
-const app = express();
+const app = express()
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
-});
+  res.send('Hello world\n')
+})
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST)
+console.log(`Running on http://${HOST}:${PORT}`)
