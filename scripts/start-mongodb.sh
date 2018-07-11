3c33199eca0542262551bd117835417c3ced122b
Kenneth Ng <ng.tszcheong@gmail.com>
2018-07-11 17:31:14 +0800

    Add start mongo scripts

---
+++ scripts/start-mongodb.sh
@@ -0,0 +1,2 @@
+docker run -d --name=dev-mongo -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=example mongo:latest
+docker run -d --name=dev-mongo-express --link dev-mongo:mongo -p 8081:8081 -e ME_CONFIG_MONGODB_ADMINUSERNAME=root -e ME_CONFIG_MONGODB_ADMINPASSWORD=example mongo-express:latest
